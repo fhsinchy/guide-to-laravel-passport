3c15f2c0053d3c9273cdc45c980cc7abfe0c4ca4
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-06-20 14:35:40 +0600

    replaced simple get routes with view routes

--- auth-server/routes/web.php
+++ auth-server/routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Route::middleware(['auth'])->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
