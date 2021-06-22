e9c01e5b164acc2fee897c20f4e6638f12ce64b1
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-06-22 20:57:11 +0600

    simplified the code for the navigarion bar

--- auth-client/resources/views/partials/_navbar.blade.php
+++ auth-client/resources/views/partials/_navbar.blade.php
@@ -1,43 +1,33 @@
 <nav class="navbar mb-5" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
-
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
         </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-menu">
         <div class="navbar-start">
-        <a href="{{ route('home') }}" class="navbar-item">
-            Home
-        </a>
+            <a href="{{ route('home') }}" class="navbar-item">Home</a>
         </div>
 
         <div class="navbar-end">
-        <div class="navbar-item">
-            <div class="buttons">
-                @if (isLoggedIn())
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
+            <div class="navbar-item">
+                <div class="buttons">
+                    @if (isLoggedIn())
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
 
-                        <a href="{{ route('logout') }}" class="button is-dark"
-                        onclick="event.preventDefault();
-                        this.closest('form').submit();">
-                            Log out
-                        </a>
-                    </form>
-                @else
-                    <a href="{{ route('redirect') }}" class="button is-light">
-                        Log in
-                    </a>
-                @endif
+                            <a href="{{ route('logout') }}" class="button is-dark"
+                            onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                                Log out
+                            </a>
+                        </form>
+                    @else
+                        <a href="{{ route('redirect') }}" class="button is-light">Log in</a>
+                    @endif
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </nav>
