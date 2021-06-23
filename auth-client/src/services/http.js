498f5d00be28961dbd9df54acc3e1d1a42f85ef3
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-06-23 14:29:13 +0600

    updated refresh token logic

--- auth-client/src/services/http.js
+++ auth-client/src/services/http.js
@@ -31,15 +31,8 @@ export default function configureAxios() {
     async (error) => {
       const originalRequest = error.config;
 
-      if (
-        error.response.status === 401
-        && originalRequest.url === originalRequest.url.includes('/oauth/token')
-      ) {
-        actions.logout();
-        router.push('/');
-        return Promise.reject(error);
       // eslint-disable-next-line no-underscore-dangle
-      } if (error.response.status === 401 && !originalRequest._retry) {
+      if (error.response.status === 401 && !originalRequest._retry) {
         // eslint-disable-next-line no-underscore-dangle
         originalRequest._retry = true;
 
@@ -53,11 +46,15 @@ export default function configureAxios() {
 
         mutations.setToken(res.data.access_token);
         window.localStorage.setItem('accessToken', res.data.access_token);
+        window.localStorage.setItem('refreshToken', res.data.refresh_token);
 
         originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
 
         return axios(originalRequest);
       }
+
+      actions.logout();
+      router.go();
       return Promise.reject(error);
     },
   );
